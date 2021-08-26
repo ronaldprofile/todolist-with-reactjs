3695a38cadef868cd9be8704df7e77cf9b8ca694
Ronald <ronaldtmprofile@gmail.com>
2021-08-26 12:02:25 -0300

    function addTodoOnState added on component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import { AddTodo } from "../AddTodo";
 import { Todos } from "../Todos";
+import { Todo } from "../Todo";
+import { NoTodos } from "../NoTodos";
+
 import { Container, Header } from "./styles";
+interface ICurrentTodoState {
+  id: number;
+  content: string;
+}
 
 export function Layout() {
+  const [currentTodo, setCurrentTodo] = useState<ICurrentTodoState[]>([]);
+
+  function addTodoOnState(todo: ICurrentTodoState) {
+    setCurrentTodo([...currentTodo, todo]);
+  }
+
   return (
     <div>
       <Header />
       <Container>
-        <AddTodo />
-        <Todos />
+        <AddTodo addTodoOnState={addTodoOnState} />
+
+        {currentTodo.length <= 0 && <NoTodos />}
+
+        <Todos>
+          {currentTodo.map(todo => (
+            <Todo key={todo.id} todo={todo} />
+          ))}
+        </Todos>
       </Container>
     </div>
   );
