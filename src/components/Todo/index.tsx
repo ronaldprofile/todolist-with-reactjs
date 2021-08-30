341fa5cb0ef0d8a3ff9f4a67cb4ca3b6b4f396aa
Ronald <ronaldtmprofile@gmail.com>
2021-08-30 12:42:38 -0300

    improving code organization

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,44 +1,62 @@
-import { Container } from "./styles";
+import checkIcon from "../../images/check.svg";
+import editIcon from "../../images/edit.svg";
+import trashIcon from "../../images/trash.svg";
+import { StatusTodo } from "../StatusTodo";
+
+import { Container, Buttons } from "./styles";
 
 interface ITodo {
-  id: number;
   content: string;
+  isCompleted: boolean;
+  key: string;
 }
 
 interface ITodoProps {
   todo: ITodo;
+  markTodoAsCompleted: (key: string) => void;
+  editTodo: (key: string) => void;
+  deleteTodo: (key: string) => void;
 }
 
-export function Todo({ todo }: ITodoProps) {
+export function Todo({
+  todo,
+  markTodoAsCompleted,
+  editTodo,
+  deleteTodo
+}: ITodoProps) {
   return (
     <Container>
-      <div className="todo">
-        <span>{todo.id}</span>
-        <strong>{todo.content}</strong>
-        <button>
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M3 6H5H21"
-              stroke="#E83F5B"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M8 6V4C8 3.46957 8.21071 2.96086 8.58579 2.58579C8.96086 2.21071 9.46957 2 10 2H14C14.5304 2 15.0391 2.21071 15.4142 2.58579C15.7893 2.96086 16 3.46957 16 4V6M19 6V20C19 20.5304 18.7893 21.0391 18.4142 21.4142C18.0391 21.7893 17.5304 22 17 22H7C6.46957 22 5.96086 21.7893 5.58579 21.4142C5.21071 21.0391 5 20.5304 5 20V6H19Z"
-              stroke="#E83F5B"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </button>
+      <strong>{todo.content}</strong>
+
+      <div className="actions">
+        <StatusTodo isCompleted={todo.isCompleted} />
+
+        <Buttons>
+          {todo.isCompleted ? (
+            <button onClick={() => deleteTodo(todo.key)} title="excluir tarefa">
+              <img src={trashIcon} alt="trash icon" />
+            </button>
+          ) : (
+            <>
+              <button onClick={() => editTodo(todo.key)} title="editar tarefa">
+                <img src={editIcon} alt="edit icon" />
+              </button>
+
+              <button
+                onClick={() => deleteTodo(todo.key)}
+                title="excluir tarefa"
+              >
+                <img src={trashIcon} alt="trash icon" />
+              </button>
+              <button
+                onClick={() => markTodoAsCompleted(todo.key)}
+                title="marcar como concluída"
+              >
+                <img src={checkIcon} alt="check icon" />
+              </button>
+            </>
+          )}
+        </Buttons>
       </div>
     </Container>
   );
--- src/components/Todo/styles.ts
+++ src/components/Todo/styles.ts
@@ -1,43 +1,62 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  .todo {
-    width: 100%;
-    padding: 2rem;
-    position: relative;
+  width: 100%;
+  padding: 2rem;
+  position: relative;
 
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  font-size: 1.6rem;
+  font-weight: 500;
+  background-color: #fff;
+  box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px,
+    rgba(17, 17, 26, 0.1) 0px 0px 8px;
+
+  &::before {
+    content: "";
+    width: 0.25rem;
+    height: 0;
+
+    position: absolute;
+    top: 0;
+    left: -1px;
+
+    border-radius: 5px;
+    background-color: #00297f;
+
+    transition: height 0.2s;
+  }
+
+  &:hover::before {
+    height: 100%;
+  }
+
+  .actions {
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    gap: 1.8rem;
+  }
+`;
 
-    font-size: 1.6rem;
-    font-weight: 500;
-    background-color: #fff;
-    box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px,
-      rgba(17, 17, 26, 0.1) 0px 0px 8px;
+export const Buttons = styled.div`
+  display: flex;
+  gap: 0.5rem;
 
-    &::before {
-      content: "";
-      width: 0.25rem;
-      height: 0;
+  button {
+    padding: 0;
+    width: 4rem;
+    height: 4rem;
 
-      position: absolute;
-      top: 0;
-      left: -1px;
+    font-size: 0;
+    background: transparent;
+    border: 1px solid #eeeeee;
 
-      border-radius: 5px;
-      background-color: #00297f;
-
-      transition: height 0.2s;
-    }
-
-    &:hover::before {
-      height: 100%;
-    }
-
-    > button {
-      background: transparent;
-      border: 0;
+    transition: transform 0.2s;
+    &:hover {
+      transform: translateY(-4px);
     }
   }
 `;
