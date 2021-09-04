5f326061f38055a11a73731846283215c1dee4a8
Ronald <ronaldtmprofile@gmail.com>
2021-09-03 21:25:39 -0300

    added a simple logo to the project

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export function Logo() {
+  return (
+    <Container>
+      todo<span>.</span>list
+    </Container>
+  );
+}
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.h1`
+  text-align: center;
+  font-size: 2.4rem;
+  color: #fff;
+
+  span {
+    font-size: 4rem;
+    color: #00ff00;
+  }
+`;
