2581953ab0d152d116cc4ed0f699f9113f443765
Ronald <ronaldtmprofile@gmail.com>
2021-09-08 11:10:26 -0300

    added the AuthContextProvider around the app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import { Layout } from "./components/Layout";
+import { Login } from "./components/Login";
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+import "./services/firebase";
+
 import { GlobalStyles } from "./styles/global";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Layout />
+      <BrowserRouter>
+        <Switch>
+          <AuthContextProvider>
+            <Route exact path="/" component={Login} />
+            <Route path="/dashboard" component={Layout} />
+          </AuthContextProvider>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
