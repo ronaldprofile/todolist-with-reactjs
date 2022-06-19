074abeae7ba5d830730db2f00ee631446682c7a4
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-06-19 10:23:28 -0300

    remove subrepo backend

--- backend/prisma/migrations/20220514000602_create_model_user/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "password_confirmation" TEXT NOT NULL
-);
